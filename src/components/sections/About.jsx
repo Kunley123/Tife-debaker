000505d4baff00d2f6a53d8146196aeac64be008
Adebanjo Adekunle <Adebanjokunle56@gmail.com>
2025-10-20 22:09:09 +0100

    Another commit on the home

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -7,17 +7,17 @@ export const About = () => {
 
             <div className="md:mx-8 md:w-full md:max-w-5xl">
                 <div className="flex flex-row rounded-xl p-8 border-black/10 border w-full mx-auto transition-all hover:shadow-lg">
-                    <div className="flex flex-col items-center justify-center pt-3">
+                    <div className="flex flex-col w-full items-center justify-center pt-3">
                         <h2 className="text-3xl font-bold mb-2 text-[#3e2c23] md:text-6xl text-center">
                             About Us
                         </h2>
-                        <p className="text-[#6b5a4c] md:text-lg sm:text-sm">
+                        <p className="text-[#6b5a4c] w-full md:text-lg sm:text-sm">
                             We are dedicated to creating delicious pasteries using the finest ingredients and recipe. <br />My story began with a passion for baking and a leve for sweet treats.
 
                         </p>
                     </div>
 
-                    <img src={aboutImg} className="p-4 w-full h-auto object-cover" alt="image" />
+                    <img src={aboutImg} className="p-4 w-[100px] md:w-[300px] h-auto object-cover" alt="image" />
                 </div>
             </div>
         </section>
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,12 +6,12 @@ export const Home = () => {
     return (
         <section id="home" className="md:min-h-screen flex items-center justify-center py-20 relaive">
             <div className="flex flex-col justify-center overflow-hidden max-w-screen  mx-4 items-center ">
-                <div className="text-center items-center justify-center flex flex-row px-4">
+                <div className="text-center items-center w-full justify-center flex flex-rows-2 px-4">
                     <div className="flex flex-col items-center w-full md:max-w-lg justify-center text-center">
                         <h1 className="text-2xl md:text-5xl text-[#3e2c23] font-bold mb-6 "> Freshly Baked Happiness Everyday</h1>
-                        <a href="#product"><button className="rounded-full mt-3 p-1 bg-[#e67e22] text-[#3e2c23] cursor-pointer text-white text-[10px] font-bold w-35 px-2">ORDER NOW</button></a>
+                        <a href="#product"><button className="rounded-full mt-3 p-1 bg-[#e67e22] text-[#3e2c23] cursor-pointer text-white text-[10px] font-bold w-25 px-2">ORDER NOW</button></a>
                     </div>
-                    <img src={cakeImg} alt="cake" className="w-full pt-4 md:max-w-lg object-cover mb-12" />
+                    <img src={cakeImg} alt="cake" className="w-[160px] md:w-[400px] pt-4 object-cover mb-12" />
                 </div>
                 <p className="text-2xl md:text-5xl animate-moveRight text-[#3e2c23] font-bold mb-6">Welcome To My Cake Shop. Freshly Baked everyday. Make Your Choice Now!</p>
             </div>
